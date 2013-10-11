a7b92b1fba1e006da9e9ea656335b06c5c4faee8
Erik Hommel <hommel@ee-atwork.nl>
2013-10-11 15:48:09 +0200

    basic version, tested locally on function level

--- README.md
+++ README.md
@@ -3,14 +3,16 @@ org.civicoop.streetformatnl
 
 CiviCRM extension to change the street_address parsing to Dutch format (street_name, street_number, all others)
 
-It only makes sense to enable this extension if the CiviCRM setting 'street address parsing' is checked. This setting can be found at Administer/Localization/Address Settings (or in Dutch"Beheren/Lokalisatie/Adresformaten) AND if the local language is set to Dutch (NL_nl)
+It only makes sense to enable this extension if the CiviCRM setting 'street address parsing' is checked. This setting can be found at Administer/Localization/Address Settings (or in Dutch"Beheren/Lokalisatie/Adresformaten) AND if the local language is set to Dutch (nl_NL)
+Also, the assumption is that either Netherlands or Belgium is set as the default country. The parsing in Dutch format will happen only for those contacts that are located in either Netherlands or Belgium.
+There will not be an error when Netherlands or Belgium is not the default country, and nothing will go wrong. It just means that there might be addresses in your database that actually would need parsing in Dutch format, but that will not happen because the country is empty.
 
 What does it do?
 -----------------
 <ol>
 <li>It changes the sequence of the street address fields in the inline edit and edit forms from street_number, street_name and all other street fields after that to the Dutch format of street_name, street_number and all other street fields after that.
 <br /><strong>Technical</strong>
-Implementation of <em>hook_civicrm_buildForm</em> to make this happen. Also, NL_nl is added as a possible language in <em>CRM/Core/BAO/Address.php</em>.
+Modified template <em>CRM/Contact/Form/Edit/Address/street_address.tpl</em>. Also, nl_NL is added as a possible language in <em>CRM/Core/BAO/Address.php</em>.
 </li>
 
 <li>It changed the way street_address is stored in the dabase to reflect the Dutch format of street_name, street_number and all other street fields after that.
@@ -24,7 +26,7 @@ Future wishes
 
 Here are a couple of things I would like to do to make this better and more generic. If you are interested in funding some of this, please contact us (helpdesk@civicoop.org, erik.hommel@civicoop.org)
 <ul>
-<li>Introduce a local file holding settings for this extension. This would include what languages to apply this for (with Dutch as default) and which countries to test for (with Netherlands as default)</li>
+<li>Introduce a local file holding settings for this extension. This would include what languages to apply this for (with Dutch as default) and which countries to test for (with Netherlands and Belgium as default)</li>
 <li>Change CRM/Core/BAO/Address.php to add possible languages based on the local settings file</li>
 <li>For the even longer term: investigate how CiviCRM is going to cope with the various address formats based on internationally recognised standards and working with standard open source tools</li>
 
--- streetformatnl.php
+++ streetformatnl.php
@@ -3,6 +3,37 @@
 require_once 'streetformatnl.civix.php';
 
 /**
+ * Implementation of hook_civicrm_buildForm
+ * 
+ * change sequence for street elements if config language is NL_nl
+ * @author Erik Hommel (erik.hommel@civicoop.org)
+ * 
+ */
+function streetformatnl_civicrm_buildForm($formName, &$form) {
+//    if ( $formName == "CRM_Contact_Form_Contact" || $formName == "CRM_Contact_Form_Inline_Address") {
+        /*
+         * check the language of the current installation and only do something if set to NL_nl
+         */
+//        $settingParams = array('return'=>'lcMessages');
+//        try{
+//            $settingApi = civicrm_api3('setting', 'getsingle', $settingParams);
+//        }
+//        catch (CiviCRM_API3_Exception $e) {
+//            $apiError = $e->getMessage();
+//            if (!isset($session)) {
+//                $session = CRM_Core_Session::singleton();
+//            }
+//            $session->setStatus("Unable to retrieve CiviCRM lcMessage setting with Setting API, check your configuration! Error from the API: $apiError", "Unable to retrieve CiviCRM language", 'error');
+//        }
+//        if (isset($settingApi['lcMessages'])) {
+//            if ($settingApi['lcMessages'] == "nl_NL") {
+//                
+//            }
+//        }
+//    }
+}
+
+/**
  * Implementation of hook_civicrm_config
  */
 function streetformatnl_civicrm_config(&$config) {
@@ -34,8 +65,95 @@ function streetformatnl_civicrm_uninstall() {
 
 /**
  * Implementation of hook_civicrm_enable
+ * 
+ * All addresses that are in the Netherlands have to be re-formatted
+ * to name/number/unit sequence
+ * 
+ * @author Erik Hommel (CiviCooP, erik.hommel@civicoop.org)
  */
+
 function streetformatnl_civicrm_enable() {
+    $toBeParsedAddresses = array();
+    /*
+     * first select addresses in Netherlands
+     */
+    $addressNlParams = array('country_id' => 1152);
+    try {
+        $apiAddressesNl = civicrm_api3('contact', 'get', $addressNlParams);
+    }
+    catch (CiviCRM_API3_Exception $e) {
+    }
+    if (isset($apiAddressesNl['values'])) {
+        $toBeParsedAddresses = $apiAddressesNl['values'];
+    }
+    /*
+     * then add addresses in Belgium
+     */
+    $addressBeParams = array('country_id' => 1020);
+    try {
+        $apiAddressesBe = civicrm_api3('contact', 'get', $addressBeParams);
+    }
+    catch (CiviCRM_API3_Exception $e) {
+    }
+    if (isset($apiAddressesBe['values'])) {
+        array_merge($toBeParsedAddresses, $apiAddressesBe['values']);
+    }
+    if (!empty($toBeParsedAddresses)) {
+        foreach ($toBeParsedAddresses as $addressKey => $address) {
+            /*
+             * check if street_name and street_number both have values. If not, 
+             * and street_address does have more than one unbroken string, split the address first
+             */
+            if (isset($address['street_address']) && !empty($address['street_address'])) {
+                if (!isset($address['street_name']) || empty($address['street_name'])) {
+                    if (!isset($address['street_number']) || empty($address['street_number'])) {                    
+                        $apiAddressParts = explode(" ", $address['street_address']);
+                        if (isset($apiAddressParts[1])) {
+                            require_once 'CRM/Core/BAO/Address.php';
+                            $parsedParts = CRM_Core_BAO_Address::parseStreetAddress($address['street_address'], 'en_US');
+                            if (isset($parsedParts['street_name'])) {
+                                $address['street_name'] = $parsedParts['street_name'];
+                            }
+                            if (isset($parsedParts['street_number'])) {
+                                $address['street_number'] = $parsedParts['street_number'];
+                            }
+                            if (isset($parsedParts['street_unit'])) {
+                                $address['street_unit'] = $parsedParts['street_unit'];
+                            }
+                        }
+                    }
+                }
+                /*
+                 * glue the address together in Dutch format
+                 */
+                $glueParams = array();
+                $apiUpdateParams = array('id' => $addressKey);
+                if (isset($address['street_name'])) {
+                    $glueParams['street_name'] = $address['street_name'];
+                    $apiUpdateParams['street_name'] = $address['street_name'];
+                    }
+                if (isset($address['street_number'])) {
+                    $glueParams['street_number'] = $address['street_number'];
+                    $apiUpdateParams['street_number'] = $address['street_number'];
+                }
+                if (isset($address['street_unit'])) {
+                    $glueParams['street_unit'] = $address['street_unit'];
+                    $apiUpdateParams['street_unit'] = $address['street_unit'];
+                }
+                if (!empty($glueParams)) {
+                    $apiUpdateParams['street_address'] = self::_glueStreetAddressNl($glueParams);
+                }
+                /*
+                 * update address with new values using API
+                 */
+                try {
+                    civicrm_api3('address', 'update', $apiUpdateParams);                    
+                }
+                catch (CiviCRM_API3_Exception $e) {
+                }
+            }
+        }
+    }
   return _streetformatnl_civix_civicrm_enable();
 }
 
@@ -68,3 +186,110 @@ function streetformatnl_civicrm_upgrade($op, CRM_Queue_Queue $queue = NULL) {
 function streetformatnl_civicrm_managed(&$entities) {
   return _streetformatnl_civix_civicrm_managed($entities);
 }
+/**
+ * function to glue street address from components in params
+ * @param array, expected street_name, street_number and possibly street_unit
+ * @return $parsedStreetAddressNl
+ */
+function _glueStreetAddressNl($params) {
+    $parsedStreetAddressNl = "";
+    /*
+     * do nothing if no street_name in params
+     */
+    if (isset($params['street_name'])) {
+        $parsedStreetAddressNl = trim($params['street_name']);
+        if (isset($params['street_number']) && !empty($params['street_number'])) {
+            $parsedStreetAddressNl .= " ".trim($params['street_number']);
+        }
+        if (isset($params['street_unit']) && !empty($params['street_unit'])) {
+            $parsedStreetAddressNl .= " ".trim($params['street_unit']);
+        }
+    }
+    return $parsedStreetAddressNl;
+}
+/**
+ * function to split street_address into components according to Dutch formats.
+ * @param streetAddress, containing parsed address in possible sequence
+ *        street_number, street_name, street_unit
+ *        street_name, street_number, street_unit
+ * @return $result, array holding street_number, street_name and street_unit
+ */
+function _splitStreetAddressNl($streetAddress) {
+    $result = array();
+    /*
+     * do nothing if streetAddress is empty
+     */
+    if (!empty($streetAddress)) {
+        /*
+         * split into parts separated by spaces
+         */
+        $addressParts = explode(" ", $streetAddress);
+        $foundStreetNumber = false;
+        $streetName = null;
+        $streetNumber = null;
+        $streetUnit = null;
+        foreach($addressParts as $partKey => $addressPart) {
+            /*
+             * if the part is numeric, there are several possibilities:
+             * - if the partKey is 0 so it is the first element, it is
+             *   assumed it is part of the street_name to cater for 
+             *   situation like 2e Wormenseweg
+             * - if not the first part and there is no street_number yet (foundStreetNumber
+             *   is false), it is assumed this numeric part contains the street_number
+             * - if not the first part but we already have a street_number (foundStreetNumber
+             *   is true) it is assumed this is part of the street_unit
+             */
+            if (is_numeric($addressPart)) {
+                if($partKey == 0) {
+                    $streetName .= $addressPart;
+                } else {
+                    if ($foundStreetNumber == false) {
+                        $streetNumber = $addressPart;
+                        $foundStreetNumber = true;
+                    } else {
+                        $streetUnit .= " ".$addressPart;
+                    }
+                }
+            } else {
+                /*
+                 * if part is not numeric, there are several possibilities:
+                 * - if the street number is found, set the whole part to streetUnit
+                 * - if there is no streetNumber yet and it is the first part, set the
+                 *   whole part to streetName
+                 * - if there is no streetNumber yet and it is not the first part,
+                 *   check all digits:
+                 *   - if the first digit is numeric, put the numeric part in streetNumber
+                 *     and all non-numerics to street_unit
+                 *   - if the first digit is not numeric, put the lot into streetName
+                 */
+                if ($foundStreetNumber == true) {
+                    $streetUnit .= " ".$addressPart;
+                } else {
+                    if ($partKey == 0) {
+                        $streetName .= $addressPart;
+                    } else {
+                        $partLength = strlen($addressPart);
+                        if (is_numeric(substr($addressPart, 0, 1))) {
+                            for ($i=0; $i<$partLength; $i++) {
+                                if (is_numeric(substr($addressPart, $i, 1))) {
+                                    $streetNumber .= substr($addressPart, $i, 1);
+                                    $foundStreetNumber = true;
+                                } else {
+                                    $streetUnit .= " ".substr($addressPart, $i, 1);
+                                }
+                            }
+                        } else {
+                            $streetName .= " ".$addressPart;
+                        }
+                    }
+                }
+            }
+        }
+        $result['street_name'] = trim($streetName);
+        $result['street_number'] = $streetNumber;
+        $result['street_unit'] = trim($streetUnit);
+    }
+    return $result;
+}
+
+
